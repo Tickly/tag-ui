cfe3c09071bc44fdf5a144838aebc13ac64808b6
tickly <807081817@qq.com>
2017-06-12 18:37:09 +0800

    添加表格统计，单元格对齐功能

--- packages/table/src/table.vue
+++ packages/table/src/table.vue
@@ -1,15 +1,23 @@
 <template>
     <table class="table" :class="tableClass">
+        <!--<colgroup>
+            <col v-for="col in _columns">
+        </colgroup>-->
         <thead v-if="columns.length">
             <tr>
-                <th v-for="col in _columns" v-text="col.label"></th>
+                <th v-for="col in _columns" :class="_headerCellClass(col)" v-text="col.label"></th>
             </tr>
         </thead>
         <tbody v-if="data.length && columns.length">
             <tr v-for="(row,rowIndex) in data">
-                <td v-for="col in _columns" v-text="_showCellContent(row,col,rowIndex)"></td>
+                <td v-for="col in _columns" :class="_cellClass(col)" v-text="_showCellContent(row,col,rowIndex)"></td>
             </tr>
         </tbody>
+        <tfoot v-if="showSummary">
+            <tr>
+                <th v-for="col in _columns" :class="_summaryCellClass(col)" v-text="_showSummaryContent(col)"></th>
+            </tr>
+        </tfoot>
     </table>
 </template>
 <script>
@@ -26,6 +34,10 @@ function parseObject(object) {
     if (object.type === 'serial') {
         object.label = '#';
     }
+    if (!object.label) {
+        object.label = object.attribute;
+    }
+
     return object;
 }
 
@@ -33,15 +45,30 @@ function parseObject(object) {
 export default {
     name: 'TagTable',
     props: {
-        data: {
-            type: Array,
-            default: [],
-        },
-        columns: Array,
-        hover: Boolean,
-        bordered: Boolean,
+        options: {
+            type: Object,
+            default: {},
+        }
     },
     computed: {
+        data() {
+            return this.options.data;
+        },
+        columns() {
+            return this.options.columns;
+        },
+        hover() {
+            return this.options.hover
+        },
+        bordered() {
+            return this.options.bordered
+        },
+        showSummary() {
+            return this.options.showSummary
+        },
+
+
+
         _columns() {
             return this.columns.map(col => {
                 var label, attribute;
@@ -69,11 +96,51 @@ export default {
         },
     },
     methods: {
+        // 显示单元格内容
         _showCellContent(row, col, rowIndex) {
             if (col.type === 'serial') {
                 return rowIndex + 1
             }
             return row[col.attribute]
+        },
+        // 显示统计行内容
+        _showSummaryContent(col) {
+            if (col.type === 'serial') return '统计'
+
+            if (!col.summary) return
+
+
+            return this.data.reduce((a, b) => {
+                a += b[col.attribute];
+                return a;
+            }, 0)
+        },
+        // 标题单元格的class
+        _headerCellClass(col) {
+            var obj = {};
+            if (col.hAlign) {
+                obj['text-' + col.hAlign] = true;
+            }
+
+            return obj;
+        },
+        // 数据单元格的class
+        _cellClass(col) {
+            var obj = {};
+            if (col.hAlign) {
+                obj['text-' + col.hAlign] = true;
+            }
+
+            return obj;
+        },
+        // 统计单元格的class
+        _summaryCellClass(col) {
+            var obj = {};
+            if (col.hAlign) {
+                obj['text-' + col.hAlign] = true;
+            }
+
+            return obj;
         }
     }
 }
--- src/examples/table.vue
+++ src/examples/table.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <h3>基础表格</h3>
+    <div class="container">
+        <h3>基础表格，带边框</h3>
         <div>
-            <tag-table bordered hover :data="tableData" :columns="columns"></tag-table>
+            <tag-table :options="tableOptions"></tag-table>
         </div>
     </div>
 </template>
@@ -10,42 +10,52 @@
 export default {
     data() {
         return {
-            tableData: [
-                {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    age: 25,
-                },
-                {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄',
-                    age: 31,
-                },
-                {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄',
-                    age: 28,
-                },
-                {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄',
-                    age: 27,
-                }
-            ],
-            columns: [
-                { type: 'serial' },
-                'date:日期',
-                'name',
-                'age',
-                {
-                    attribute: 'address',
-                    label: '地址',
-                }
-            ],
+            tableOptions: {
+                bordered: true,
+                hover: true,
+                showSummary: true,
+                data: [
+                    {
+                        date: '2016-05-02',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄',
+                        age: 25,
+                    },
+                    {
+                        date: '2016-05-04',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1517 弄',
+                        age: 31,
+                    },
+                    {
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄',
+                        age: 28,
+                    },
+                    {
+                        date: '2016-05-03',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1516 弄',
+                        age: 27,
+                    }
+                ],
+                columns: [
+                    { type: 'serial' },
+                    'date:日期',
+                    'name',
+                    // 'age',
+                    {
+                        attribute: 'age',
+                        summary: true,
+                        hAlign: 'center',
+                    },
+                    {
+                        attribute: 'address',
+                        label: '地址',
+                    }
+                ],
+            },
         }
     }
 }
